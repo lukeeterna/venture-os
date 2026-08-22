a94cb27381393621bee5aa1203ae223cf211fb0d
lukeeterna <gianlucanewtech@gmail.com>
2026-08-22 21:51:26 +0200

    fix(mt3d): load edge-safe crest conformal layer

--- ventures/run_20260711_161411/configurator/src/bootstrap.js
+++ ventures/run_20260711_161411/configurator/src/bootstrap.js
@@ -3,4 +3,5 @@ import "./sportswear-medusa-bridge.js";
 await import("./app.js");
 const teamOrder = import("./team-order.js");
 await import("./football-realism.js");
+await import("./football-crest-conformal.js");
 await teamOrder;
