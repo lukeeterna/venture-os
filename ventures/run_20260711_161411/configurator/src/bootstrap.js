a8999b25b01bf3a2d2c19d2607aff6a415bdf3d7
lukeeterna <gianlucanewtech@gmail.com>
2026-08-22 21:46:09 +0200

    feat(sportswear): activate Medusa pricing bridge

--- ventures/run_20260711_161411/configurator/src/bootstrap.js
+++ ventures/run_20260711_161411/configurator/src/bootstrap.js
@@ -1,4 +1,5 @@
 import "./football-realism-prelude.js";
+import "./sportswear-medusa-bridge.js";
 await import("./app.js");
 const teamOrder = import("./team-order.js");
 await import("./football-realism.js");
