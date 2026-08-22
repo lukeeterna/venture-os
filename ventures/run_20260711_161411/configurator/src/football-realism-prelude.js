fc52d7e96782185316daddaa565a863e62962ec0
lukeeterna <gianlucanewtech@gmail.com>
2026-08-22 19:00:47 +0200

    fix(mt3d): expose active camera for full-kit framing

--- ventures/run_20260711_161411/configurator/src/football-realism-prelude.js
+++ ventures/run_20260711_161411/configurator/src/football-realism-prelude.js
@@ -6,5 +6,15 @@ if (!window.__footballRealismCaptureInstalled) {
     window.__footballRealismScene = this;
     return sceneAdd.apply(this, objects);
   };
+
+  const rendererRender = THREE.WebGLRenderer.prototype.render;
+  THREE.WebGLRenderer.prototype.render = function (renderScene, camera) {
+    if (renderScene === window.__footballRealismScene && camera?.isPerspectiveCamera) {
+      window.__footballRealismCamera = camera;
+      window.__footballRealismRenderer = this;
+    }
+    return rendererRender.call(this, renderScene, camera);
+  };
+
   window.__footballRealismCaptureInstalled = true;
 }
