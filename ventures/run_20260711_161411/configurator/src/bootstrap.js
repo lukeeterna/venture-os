d8a87a48a7f1ed951889910b23f111e9e8b6a436
lukeeterna <gianlucanewtech@gmail.com>
2026-08-22 21:53:04 +0200

    feat(sportswear): load fabric size availability guard

--- ventures/run_20260711_161411/configurator/src/bootstrap.js
+++ ventures/run_20260711_161411/configurator/src/bootstrap.js
@@ -5,3 +5,4 @@ const teamOrder = import("./team-order.js");
 await import("./football-realism.js");
 await import("./football-crest-conformal.js");
 await teamOrder;
+await import("./team-size-availability.js");
