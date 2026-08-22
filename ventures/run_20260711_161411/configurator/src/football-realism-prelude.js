88d140930437d17946e55a866ead68075795380b
lukeeterna <gianlucanewtech@gmail.com>
2026-08-22 21:37:33 +0200

    fix(mt3d): capture football camera without render timing

--- ventures/run_20260711_161411/configurator/src/football-realism-prelude.js
+++ ventures/run_20260711_161411/configurator/src/football-realism-prelude.js
@@ -3,15 +3,34 @@ import * as THREE from "three";
 if (!window.__footballRealismCaptureInstalled) {
   const sceneAdd = THREE.Scene.prototype.add;
   THREE.Scene.prototype.add = function (...objects) {
-    window.__footballRealismScene = this;
+    const names = objects.map((object) => object?.name).filter(Boolean);
+    if (names.includes("football-kit") || names.includes("customization-decals")) {
+      window.__footballRealismScene = this;
+    }
     return sceneAdd.apply(this, objects);
+  };
+
+  const capturePerspectiveCamera = (camera) => {
+    if (camera?.isPerspectiveCamera) window.__footballRealismCamera = camera;
+  };
+
+  const cameraLookAt = THREE.PerspectiveCamera.prototype.lookAt;
+  THREE.PerspectiveCamera.prototype.lookAt = function (...args) {
+    capturePerspectiveCamera(this);
+    return cameraLookAt.apply(this, args);
+  };
+
+  const cameraProjection = THREE.PerspectiveCamera.prototype.updateProjectionMatrix;
+  THREE.PerspectiveCamera.prototype.updateProjectionMatrix = function (...args) {
+    capturePerspectiveCamera(this);
+    return cameraProjection.apply(this, args);
   };
 
   const rendererRender = THREE.WebGLRenderer.prototype.render;
   THREE.WebGLRenderer.prototype.render = function (renderScene, camera) {
     if (renderScene?.isScene && camera?.isPerspectiveCamera) {
-      window.__footballRealismScene = renderScene;
-      window.__footballRealismCamera = camera;
+      if (renderScene.getObjectByName?.("football-kit")) window.__footballRealismScene = renderScene;
+      capturePerspectiveCamera(camera);
       window.__footballRealismRenderer = this;
     }
     return rendererRender.call(this, renderScene, camera);
